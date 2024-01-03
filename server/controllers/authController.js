0ff06bb397f10b07a403dbcc9dfcf8adb773c299
Sansrita <sansritasaha@gmail.com>
2024-01-03 22:53:11 +0530

    update

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,36 @@
 const test = (req, res) => {
     res.json('test is working')
 }
+
+const registerUser = (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        //check if name was entered
+
+        if (!name) {
+            return res.json({
+                error: 'name is required'
+
+            })
+        };
+
+        //check if password is good
+
+        if (!password || password.length < 6) {
+            return res.json({
+                error: "Password is required and it should at least contain 6 characters"
+            })
+        }
+
+
+    } catch (error) {
+
+    }
+
+}
 module.exports = {
-    test
+    test,
+    registerUser
 
 }
